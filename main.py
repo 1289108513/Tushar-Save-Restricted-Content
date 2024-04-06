a6cf9188c5b4cd06c6f6a135c8f48a0d27b0d0c3
Tushar <165200812+1289108513@users.noreply.github.com>
2024-04-06 19:53:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def progress(current, total, message, type):
 
 
 # start command
-@bot.on_message(filters.command(["start"]))
+@bot.on_message(filters.command(["tushar"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, Which is Deploy by TUSHAR , I can send you restricted content by it's post link__**\n\n{USAGE}",
 	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🤖BOT MADE BY TUSHAR🇮🇳", url="https://t.me/Tushar_1665")]]), reply_to_message_id=message.id)
@@ -245,37 +245,7 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
 		msg.text
 		return "Text"
 	except: pass
-
-
-USAGE = """**FOR PUBLIC CHATS**
-
-**__just send post/s link__**
-
-**FOR PRIVATE CHATS**
-
-**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__**
-
-**FOR BOT CHATS**
-
-**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
-
-```
-https://t.me/b/botusername/4321
-```
-
-**MULTI POSTS**
-
-**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-**__note that space in between doesn't matter__**
-"""
+		
 
 
 # infinty polling
