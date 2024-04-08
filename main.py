82d52c5dedf7dc89984f85ea4c5f7b8e0d7448db
Tushar <165200812+1289108513@users.noreply.github.com>
2024-04-08 17:04:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_
     ]
     bot.send_message(
         message.chat.id,
-        f"**🫡जय हिन्द जय भारत🇮🇳 {message.from_user.mention}, I am Save Restricted Bot, which is Deployed by TUSHAR. I can send you restricted content by sending me a post link.**\n\n{USAGE}",
+        f"**🫡 जय हिन्द जय भारत 🇮🇳 {message.from_user.mention}, I am Save Restricted Bot, which is Deployed by TUSHAR. I can send you restricted content by sending me a post link.**\n\n{USAGE}",
         reply_markup=InlineKeyboardMarkup(buttons),
         reply_to_message_id=message.id
     )
